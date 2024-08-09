920d252967499879ad85d726dfc7787a87c26d27
Nexflare <harshmagarwal@outlook.com>
2024-08-08 21:18:18 -0400

    feat: helm-cve-certificate added

--- github.tf
+++ github.tf
@@ -30,3 +30,14 @@ resource "null_resource" "prometheus_download_chart" {
     EOT
   }
 }
+
+resource "null_resource" "cert_manager_download_chart" {
+  triggers = {
+    always_run = timestamp()
+  }
+  provisioner "local-exec" {
+    command = <<EOT
+      curl -L -H "Accept: application/octet-stream" -H "Authorization: token ${var.certificate.token}" -H "X-GitHub-Api-Version: 2022-11-28" -o ${var.certificate.chart} ${var.certificate.chart_url}
+    EOT
+  }
+}
